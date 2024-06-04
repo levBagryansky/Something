c8c3fd8bdd3cc29ee0cf703c4b17fa45e4c9fd81
levBagryansky <28lev11@gmail.com>
2024-06-05 01:04:39 +0300

    fight with gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tardis3/Something
-# Build directory: /home/tardis3/Something/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("test")
--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tardis3/Something/test
-# Build directory: /home/tardis3/Something/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/tardis3/Something/cmake-build-debug/test/regfile-test[1]_include.cmake")
